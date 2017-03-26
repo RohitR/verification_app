0e43f64c5b7048574eb2662418da971f843be2dc
Rohith <rohithr.krish@gmail.com>
2017-03-27 04:31:01 +0530

    default url for email changed

--- config/initializers/app_setup.rb
+++ config/initializers/app_setup.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-config.action_mailer.default_url_options = { :host => "localhost:3000" }
+config.action_mailer.default_url_options = { :host => "https://ancient-beyond-38861.herokuapp.com" }
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
